7ee3624d6dbc66e96a7bb6963ebb04b1c13214ee
Mario Patronelli <mario@raffles.com.ar>
2019-02-19 17:20:12 +0000

    Add Disclaimer to site footer

--- Resources/Javascript/components/layout/SiteFooter.vue
+++ Resources/Javascript/components/layout/SiteFooter.vue
@@ -7,7 +7,7 @@
         style="opacity:0;"
       />
       <div class="container p-t30">
-        <div class="row">
+        <div class="row mb-4">
           <div class="col-md-3 wt-footer-bot-left">
             <RouterLink :to="{ name: 'Home' }">
               <img
@@ -50,6 +50,12 @@
                 />
               </li>
             </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 text-justify">
+            <h6 class="pt-4" style="border-top:1px solid;">DISCLAIMER</h6>
+            <p>Las inversiones en real estate y sus activos subyacente requieren de atención constante y asesoramiento profesional. El rendimiento pasado no es un indicador de los resultados futuros. Invertir siempre implica un riesgo para su capital. Le sugerimos No invierta más de lo que esté dispuesto a perder. Es responsabilidad del usuario leer con detenimiento las condiciones generales y/o contratos proporcionados por cada patrocinador de negocio (desarrollistas inmobiliarios, fondo de inversiones o broker), puesto que los mismos no forman parte de la relación entre el usuario y LinkRE.biz, sino de la relación jurídica entre el usuario y el patrocinador del negocio. Desde el momento en el que el usuario accede al entorno del patrocinador del negocio, la relación jurídica y comercial correspondiente a las acciones que el usuario pretende realizar se materializa entre el patrocinador del negocio  y el usuario, sin la participación de Linkre.biz. En ningún caso Linkre.biz actúa por cuenta y en nombre del usuario. Linkre.biz únicamente pone a disposición del usuario información provista y generada por el patrocinador del negocio, junto a determinadas señales técnicas que llegan diariamente y de forma masiva desde la Aplicación de Link.biz. El usuario debe asesorarse con su equipo de asesores, deslindando de toda responsabilidad la posible perdida total o parcial de su inversión, ya que para ello acepta y reconoce que conoce los riesgo del operar en la compra, vente, financiación  inversion en activos de real estate.</p>
           </div>
         </div>
       </div>
